db5b3ce8d5f86e6628a3a9846cf2943f757c5d1e
mrinal-crypto <www.mrinal.phsa@gmail.com>
2024-02-27 22:36:33 +0530

    firebase web ui added

--- CAR_TRANSMITTER.ino
+++ CAR_TRANSMITTER.ino
@@ -1,9 +1,41 @@
+#include <Arduino.h>
+#include <WiFi.h> //works for only esp32
+#include <WiFiManager.h> //works for only esp32
+#include <FirebaseESP8266.h> //works for both esp32 and esp8266
+#include <ArduinoJson.h>
+#include <FastLED.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+#include <Preferences.h>
+#include "SPIFFS.h"
+#include <FS.h>
+
+
+TaskHandle_t Task2;
+SemaphoreHandle_t variableMutex;
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  variableMutex = xSemaphoreCreateMutex();
+  xTaskCreatePinnedToCore(
+    loop2,
+    "Task2",
+    100000,
+    NULL,
+    1,
+    &Task2,
+    1);
+  delay(500);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop2(void * parameter) {
+  for (;;) {
 
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
+
+void loop() {
+  
 }
